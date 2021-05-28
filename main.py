135653e1866b2fd8a27c49ead2cf5218b8523f99
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-28 06:15:29 +0000

    indicate more lines

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # lp3thw p. 142
 
-def zzz():
+def z():
 
   print("05 1")
   yield(1)
@@ -11,6 +11,10 @@ def zzz():
   print("11 2")
   yield(2)
 
+  print("14 end")
+# end of function zzz()
 
-for i in zzz():
-  print("16 i =", i)
+
+print("18 start the for loop")
+for i in z():
+  print("20 i =", i)
